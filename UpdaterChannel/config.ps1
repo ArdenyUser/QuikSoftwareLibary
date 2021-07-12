5ed751a667dc6d1aec4705a369adaae4520c8d11
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-07-12 09:16:49 -0400

    Create config.ps1

---
+++ UpdaterChannel/config.ps1
@@ -0,0 +1,510 @@
+# 0 in the file = you cannot use this program, 1 allows you to use this
+wget ConfigURL -UseBasicParsing -OutFile confige
+$confige = Get-Content -Path .\confige
+cmd if ( $config -eq 1 )
+{
+cd C:\
+mkdir CCdatQuik
+cd C:\CCdatQuik
+mkdir LOT
+mkdir LOT1
+mkdir LOT2
+mkdir LOT3
+mkdir LOT4
+mkdir LOT5
+mkdir LOT6
+mkdir LOT7
+mkdir LOT8
+mkdir LOT9
+cd C:\CCdatQuik\LOT
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT
+cd C:\CCdatQuik\LOT1
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT1
+cd C:\CCdatQuik\LOT2
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT2
+cd C:\CCdatQuik\LOT3
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT3
+cd C:\CCdatQuik\LOT4
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT4
+cd C:\CCdatQuik\LOT5
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT5
+cd C:\CCdatQuik\LOT6
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT6
+cd C:\CCdatQuik\LOT7
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT7
+cd C:\CCdatQuik\LOT8
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT8
+cd C:\CCdatQuik\LOT9
+# Do the below command to make every file in this lot
+Set-Content -Path .\File -Value 'Data'
+cmd if exist 1.py (
+    py 1.py
+)
+cmd if exist 2.py (
+    py 2.py
+)
+cmd if exist 3.py (
+    py 3.py
+)
+cmd if exist 1.class (
+    java 1.class
+)
+cmd if exist 2.class (
+    java 2.class
+)
+cmd if exist 3.class (
+    java 3.class
+)
+cmd if exist 1.java (
+    java 1.java
+)
+cmd if exist 2.java (
+    java 2.java
+)
+cmd if exist 3.java (
+    java 3.java
+)
+cmd if exist 1.jar (
+    java 1.jar
+)
+cmd if exist 2.jar (
+    java 2.jar
+)
+cmd if exist 3.jar (
+    java 3.jar
+)
+cmd if exist 1.cmd (
+    1.cmd
+)
+cmd if exist 2.cmd (
+    2.cmd
+)
+cmd if exist 3.cmd (
+    3.cmd
+)
+rmdir -Force C:\CCdatQuik\LOT9
+pause
+}
